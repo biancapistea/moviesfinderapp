53d24a46106d13e111766b0f6bbbf181d965d94b
biancapistea <biancapistea@gmail.com>
2024-05-20 12:37:36 +0300

    refactor code

--- app/src/main/java/com/example/moviesfinderapp/ui/screens/auth/AuthenticatorScreen.kt
+++ app/src/main/java/com/example/moviesfinderapp/ui/screens/auth/AuthenticatorScreen.kt
@@ -11,16 +11,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableDoubleStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
